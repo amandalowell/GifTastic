18fd0e480b9c75dfdf5e948dd8fd86de4856bc3a
Amanda Lowell <amandalowell2016@gmail.com>
2019-06-10 20:15:18 -0500

    topics var

---
+++ app.js
@@ -0,0 +1,5 @@
+var topics, newButton, newTopic, queryURL, flowerName, results, flowerDiv, rating, gifShow, state;
+
+
+topics = ["BubbleGum", "Fallout", "Yoga", "Bathtub", "Stephen King" ,"Hiking", "Piano", "Flower", "Strawberry"];
+
